d3d262a18d04a0854d9fa16601ca5e2477cfe5a1
lsmucassi <lindasmucassi@gmail.com>
2021-02-19 01:58:23 +0200

    layout: card need fixing

--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,37 @@
 import Image from "next/image";
+import styles from '../styles/Home.module.css'
 
 export default function Card() {
     return(
         <>
-            <div className={Styles.card_wrap} >
-                <div className={styles.card_item} >
-                    <dic className={styles.card_inner}>
-                        <div className={styles.card_img} >
-                        <Image  src="/images/profiles/pp-1.jpg" alt="me" width="64" height="64" />
-                        </div>
-                        <div className={styles.pet_name}>Scooby Doo</div>
-                        <div className={styles.pet_role}>Jumper this one</div>
-                        <a href
-                    </dic>
+            <div className={styles.card_container}>
+                <span className={styles.pro}>PRO</span>
+                <img className={styles.round} src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+                <h3>Ricky Park</h3>
+                <h6>New York</h6>
+                <p>User interface designer and <br/> front-end developer</p>
+                <div className={styles.buttons}>
+                    <button className={styles.primary}>
+                        Message
+                    </button>
+                    {/* <button className={styles.primary, styles.ghost}>
+                        Following
+                    </button> */}
+                </div>
+                <div className={styles.skills}>
+                    <h6>Skills</h6>
+                    <ul>
+                        <li>UI / UX</li>
+                        <li>Front End Development</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node</li>
+                    </ul>
                 </div>
             </div>
+
         </>
     )
 }
