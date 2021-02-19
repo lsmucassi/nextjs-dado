47d081759e3a5261c175685ab361d13fe795f6a2
lsmucassi <lindasmucassi@gmail.com>
2021-02-19 02:01:09 +0200

    layout: card - fixing position

--- components/Card.js
+++ components/Card.js
@@ -10,26 +10,6 @@ export default function Card() {
                 <h3>Ricky Park</h3>
                 <h6>New York</h6>
                 <p>User interface designer and <br/> front-end developer</p>
-                <div className={styles.buttons}>
-                    <button className={styles.primary}>
-                        Message
-                    </button>
-                    {/* <button className={styles.primary, styles.ghost}>
-                        Following
-                    </button> */}
-                </div>
-                <div className={styles.skills}>
-                    <h6>Skills</h6>
-                    <ul>
-                        <li>UI / UX</li>
-                        <li>Front End Development</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Node</li>
-                    </ul>
-                </div>
             </div>
 
         </>
