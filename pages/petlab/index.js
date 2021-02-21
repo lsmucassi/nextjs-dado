2b2ca3b858aff032e9f3ef9c787ba074c7b1d149
lsmucassi <lindasmucassi@gmail.com>
2021-02-21 18:53:56 +0200

    app: class to className on cantact us(hoof hoof)

--- pages/petlab/index.js
+++ pages/petlab/index.js
@@ -2,38 +2,6 @@ import styles from '../../styles/Home.module.css'
 import Card from '../../components/Card'
 import { fetchEntries } from '../api/content'
 
-// const petList = []
-const client = require('contentful').createClient({
-    space: 'bnvvhtbeth06',
-    accessToken: 'fjCiI01g71Uk7oMNTgv419qQGI2E0kOLdm8JbCL_iUs'
-})
-
-
-export async function getStaticProps() {
-    const petList = await client
-    .getEntries({ content_type: "blogPost" })
-    .then((response) => response.items)
-
-    return {
-        props: {
-            petList,
-        },
-    }
-    
-    
-//     const res = await fetchEntries()
-//     petList = await res.map((p) => {
-//     return p.fields
-// })
-
-//     return {
-//         props: {
-//             petList,
-//         },
-//     }
-}
-
-
 export default function Petlab( { petList }) {
     console.log(petList)
     return(
@@ -52,28 +20,20 @@ export default function Petlab( { petList }) {
     )
 }
 
+// const petList = []
+const client = require('contentful').createClient({
+    space: 'bnvvhtbeth06',
+    accessToken: 'fjCiI01g71Uk7oMNTgv419qQGI2E0kOLdm8JbCL_iUs'
+})
 
-// inside your component markup, pull `posts` from props
+export async function getStaticProps() {
+    const petList = await client
+    .getEntries({ content_type: "blogPost" })
+    .then((response) => response.items)
 
-{/* <div className="posts">
-    {posts.map((p) => {
-    return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-    })}
-</div> */}
-
-// at the bottom of your component file
-
-
-// const getStaticProps = async () => {
-//     const client = require('contentful').createClient({
-//         space: 'bnvvhtbeth06',
-//         accessToken: 'fjCiI01g71Uk7oMNTgv419qQGI2E0kOLdm8JbCL_iUs'
-//     })
-//     const pets = await client.getEntries()
-//     console.log(pets.items)
-//     return {
-//         props: {
-//             petList: pets.items || []
-//         }
-//     }
-// }
+    return {
+        props: {
+            petList,
+        },
+    }
+}
