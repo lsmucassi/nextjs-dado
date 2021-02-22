f7f273bc9ef13a9f8420cb5cf9cb85c34b3bc1e3
lsmucassi <lindasmucassi@gmail.com>
2021-02-22 18:32:56 +0200

    app: render cms data

--- components/Snap.js
+++ components/Snap.js
@@ -1,18 +1,20 @@
-import Image from "next/image";
 import styles from '../styles/Home.module.css'
 
 export default function Snap({ pets }) {
-    console.log(pets)
     return(
         <>
             <div className={styles.card_container}>
-                <div className={styles.round}>
-                    {/* <Image className={styles.round} src={pets.petImage.fields.file.fileName} layout='fixed' alt="dogo" width="164" height="164"/> */}
-                </div>
-                <h3>{pets.petName}</h3>
+                
+                <h3>{pets.petName}
+                    <h6>Age:[{pets.petAge}]</h6>
+                </h3>
+                
                 <h6>{pets.petRole}</h6>
-                <a href="">read more about me</a>
+                <hr />
+                <p>{pets.bio}</p>
             </div>
+
+            
         </>
     )
 }
